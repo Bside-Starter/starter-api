5ac32ab9412305471593de840994b722a9112497
Duhan Mo <devduhanmo@gmail.com>
2022-12-27 13:36:17 +0900

    first-commit

---
+++ src/main/java/com/bside/starterapi/StarterApiApplication.java
@@ -0,0 +1,13 @@
+package com.bside.starterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bside/starterapi/StarterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bside.starterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
