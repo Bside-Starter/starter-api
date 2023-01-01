4c7be11f2b796f069e21dcb0c894a56eeaa1ab76
Duhan Mo <devduhanmo@gmail.com>
2023-01-01 23:46:29 +0900

    feat: UserDetails, UserDetailsService Implements that used by Security

---
+++ src/main/java/com/bside/starterapi/config/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.bside.starterapi.config.auth;
+
+import com.bside.starterapi.domain.User;
+import com.bside.starterapi.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Fount with username: " + username));
+
+        return UserDetailsImpl.from(user);
+    }
+}
