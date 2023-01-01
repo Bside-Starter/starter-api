a5e3984fa05edce171dc31fe64d70eede55f6534
Duhan Mo <devduhanmo@gmail.com>
2023-01-01 23:03:34 +0900

    feat: Add Domain for JWT

---
+++ src/main/java/com/bside/starterapi/domain/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.bside.starterapi.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
---
+++ src/main/java/com/bside/starterapi/domain/User.java
@@ -0,0 +1,32 @@
+package com.bside.starterapi.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
