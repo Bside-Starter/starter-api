68247856f13e7485c41f82115efb6446a9f9aec6
Duhan Mo <moduhan95@gmail.com>
2023-01-20 18:06:37 +0900

    test: fix springboottest profile

--- src/test/java/com/bside/starterapi/StarterApiApplicationTests.java
+++ src/test/java/com/bside/starterapi/StarterApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.bside.starterapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StarterApiApplicationTests {
 
     @Test
